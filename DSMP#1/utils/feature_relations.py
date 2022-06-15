7ae5b7aa7038e16c02098e59d773d677b0e18133
Siarhei Sintsou <Siarhei.Sintsou@burberry.com>
2022-06-15 09:49:38 +0300

    add more crosstabs

--- DSMP#1/utils/feature_relations.py
+++ DSMP#1/utils/feature_relations.py
@@ -1,10 +1,10 @@
-import scipy.stats as ss
 import numpy as np
 import pandas as pd
+import scipy.stats as ss
 
 
 def cramers_v(x, y):
-    """Implements Cramér's V (sometimes referred to as Cramér's phi and denoted as φc). It is
+    """ Implements Cramér's V (sometimes referred to as Cramér's phi and denoted as φc). It is
     a measure of association between two nominal variables, giving a value between 0 and +1 (inclusive). 
     It is based on Pearson's chi-squared statistic and was published by Harald Cramér in 1946.
     
@@ -56,8 +56,10 @@ def theils_u(x, y):
     s_xy = conditional_entropy(x,y)
     x_counter = Counter(x)
     total_occurrences = sum(x_counter.values())
+    
     p_x = list(map(lambda n: n/total_occurrences, x_counter.values()))
     s_x = ss.entropy(p_x)
+    
     if s_x == 0:
         return 1
     else:
@@ -65,7 +67,7 @@ def theils_u(x, y):
     
 
 def correlation_ratio(categories, measurements):
-    """The correlation ratio is a measure of the curvilinear relationship between the statistical
+    """ The correlation ratio is a measure of the curvilinear relationship between the statistical
     dispersion within individual categories and the dispersion across the whole population or sample.
     The measure is defined as the ratio of two standard deviations representing these types of variation.
     https://en.wikipedia.org/wiki/Correlation_ratio
@@ -78,20 +80,24 @@ def correlation_ratio(categories, measurements):
     https://towardsdatascience.com/the-search-for-categorical-correlation-a1cf7f1888c9
     """
     fcat, _ = pd.factorize(categories)
-    cat_num = np.max(fcat)+1
+    cat_num = np.max(fcat) + 1
     y_avg_array = np.zeros(cat_num)
     n_array = np.zeros(cat_num)
+    
     for i in range(0,cat_num):
         cat_measures = measurements[np.argwhere(fcat == i).flatten()]
         n_array[i] = len(cat_measures)
         y_avg_array[i] = np.average(cat_measures)
-    y_total_avg = np.sum(np.multiply(y_avg_array,n_array))/np.sum(n_array)
-    numerator = np.sum(np.multiply(n_array,np.power(np.subtract(y_avg_array,y_total_avg),2)))
-    denominator = np.sum(np.power(np.subtract(measurements,y_total_avg),2))
+        
+    y_total_avg = np.sum(np.multiply(y_avg_array, n_array)) / np.sum(n_array)
+    numerator = np.sum(np.multiply(n_array, np.power(np.subtract(y_avg_array, y_total_avg), 2)))
+    denominator = np.sum(np.power(np.subtract(measurements, y_total_avg), 2))
+    
     if numerator == 0:
         eta = 0.0
     else:
         eta = np.sqrt(numerator/denominator)
+
     return eta
 
 
